636c38852ee5d351c53c3ff3eeddb82ca09390a6
John Wolfe <johnwolfe@Johns-MacBook-Pro-2.local>
2018-01-28 15:43:16 -0500

    app

--- src/App.js
+++ src/App.js
@@ -9,6 +9,25 @@ class App extends Component {
     stories: [{id: 1, title: "hello world", edit: false}, {id: 2, title: "hello world2", edit: true}, {id: 3, title: "hello world3", edit: false}]
   }
 
+  handleToggle = (event) => {
+    const index = this.state.stories.findIndex((story) => story.id == event.target.value);
+    const foundStory = this.state.stories.find((story) => story.id == event.target.value)
+  event.target.name == "Save" ? foundStory.edit = false : foundStory.edit = true
+  debugger
+  const updatedStories = [
+    ...this.state.stories.slice(0, index),
+    foundStory,
+    ...this.state.stories.slice(index + 1)
+  ];
+  this.setState({
+    stories: updatedStories
+  }) 
+}
+
+handleDelete = (event) => {
+  debugger
+}
+
   render() {
     return (
       <div className="App">
