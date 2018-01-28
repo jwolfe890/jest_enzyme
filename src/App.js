7ef6d0c36ee219ee283d22c8550547aedf22204d
John Wolfe <johnwolfe@Johns-MacBook-Pro-2.local>
2018-01-28 15:42:54 -0500

    render

--- src/App.js
+++ src/App.js
@@ -2,3 +2,20 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Map from './Map'
+
+class App extends Component {
+
+  state = {
+    stories: [{id: 1, title: "hello world", edit: false}, {id: 2, title: "hello world2", edit: true}, {id: 3, title: "hello world3", edit: false}]
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Map stories={this.state.stories} handleSubmit={this.handleSubmit} handleToggle={this.handleToggle} handleSave={this.handleToggle} />
+      </div>
+    );
+  }
+}
+
+export default App;
